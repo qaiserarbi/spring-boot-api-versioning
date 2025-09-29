be6bf22d5e254024b52e59d38f9905032952c04a
Qaiser <qaiserarbi@gmail.com>
2025-09-29 21:35:11 +0300

    swagger configuration

--- src/main/java/com/demo/spring_boot_api_versioning/SpringBootApiVersioningApplication.java
+++ src/main/java/com/demo/spring_boot_api_versioning/SpringBootApiVersioningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootApiVersioningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiVersioningApplication.class, args);
+	void main() {
+		SpringApplication.run(SpringBootApiVersioningApplication.class);
 	}
 
 }
